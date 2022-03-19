3b886c79ad1b2eb0b6880a14c444a18a39180182
eniallator <eniallatormc@gmail.com>
2022-03-19 14:32:52 +0000

    Added the rental and reservation models

--- apps/rental/admin.py
+++ apps/rental/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Rental, Reservation
+
+
+class RentalAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ("rental_name", "previous_reservation", "checkin", "checkout")
+
+    def previous_reservation(self, obj):
+        return obj.previous_reservation
+
+    def rental_name(self, obj):
+        return obj.rental.name
+
+
+admin.site.register(Rental, RentalAdmin)
+admin.site.register(Reservation, ReservationAdmin)
--- apps/rental/models.py
+++ apps/rental/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Rental(models.Model):
+    name = models.CharField(max_length=255, help_text="Name of the rental")
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+    # Choosing set null, as these are transactions records
+    rental = models.ForeignKey(
+        Rental, null=True, related_name="reservations", on_delete=models.SET_NULL
+    )
+
+    checkin = models.DateField(help_text="Date of check in")
+    # Checking out may be in the future, so may not be certain which is why it's None
+    checkout = models.DateField(help_text="Date of check out", blank=True, null=True)
+
+    @property
+    def previous_reservation(self):
+        previous_reservations = Reservation.objects.filter(
+            rental=self.rental, checkin__lt=self.checkin
+        )
+        if not previous_reservations.exists():
+            return None
+        return previous_reservations.order_by("-checkin").first()
